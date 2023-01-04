62ffc0369b1cc3a41a1ba6feb5f77a95203a7fc4
Vertical <ambintasso@gmail.com>
2023-01-04 20:46:26 +0530

    tabswitch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("yoooooooo");
+import { initialPageLoad } from "./pageload";
+import { menu } from "./menu";
+import { contact } from "./contact";
+
+initialPageLoad();
+
+let tabSwitch = (() => {
+  const homeTab = document.querySelector(".home");
+  homeTab.addEventListener("click", initialPageLoad);
+
+  const menuTab = document.querySelector(".menu");
+  menuTab.addEventListener("click", menu);
+
+  const contactTab = document.querySelector(".contact");
+  contactTab.addEventListener("click", contact);
+})();
