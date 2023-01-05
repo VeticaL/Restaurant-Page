d4787f91ab36d24d2939f2f1d8d41ad57bbb41c5
Vertical <ambintasso@gmail.com>
2023-01-05 12:24:56 +0530

    build adj

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'assest/resourse',
-      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
     ],
   },
 };
